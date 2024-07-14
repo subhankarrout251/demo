4705238e498e7d1ab8baaa9ffc1adc83111c5620
Subhankar Rout <118932561+subhankarrout251@users.noreply.github.com>
2024-07-14 17:17:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const connectDB = async () => {
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://6693babb2fc3db3caf8a04af--sweet-mandazi-016f94.netlify.app", credentials: true }));
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
